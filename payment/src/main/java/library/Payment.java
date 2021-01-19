0309237e2f0b7b41dc988ce09a24eb86a9ec6d16
hsm0320 <75401893+hsm0320@users.noreply.github.com>
2021-01-19 23:37:57 +0900

    부하테스트 주석해제

--- payment/src/main/java/library/Payment.java
+++ payment/src/main/java/library/Payment.java
@@ -22,13 +22,13 @@ public class Payment {
         paid.publishAfterCommit();
 
         //부하테스트 시간끌기
-        /*
+        
         try {
             Thread.currentThread().sleep((long) (400 + Math.random() * 220));
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        */
+        
     }
 
     @PostUpdate
