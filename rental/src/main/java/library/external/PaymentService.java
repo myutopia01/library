8e68bf38802ed53a3da6a108673cc824f30773cd
nettik-sk <75237785+nettik-sk@users.noreply.github.com>
2021-01-19 23:48:45 +0900

    url 수정

--- rental/src/main/java/library/external/PaymentService.java
+++ rental/src/main/java/library/external/PaymentService.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Date;
 //@FeignClient(name="payment", url="http://payment:8080")
-@FeignClient(name="payment", url="${api.delivery.url}")
+@FeignClient(name="payment", url="${api.payment.url}")
 public interface PaymentService {
 
     @RequestMapping(method= RequestMethod.POST, path="/payments")//, fallback = PaymentServiceFallback.class)
